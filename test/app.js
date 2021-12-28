db4a61a6c5b6df33aedd9a959dd95a11baf63668
ahmedibrahim404 <ahmedie20022011@gmail.com>
2021-12-28 23:41:06 +0200

    feat: Integrated Unit testing for the API

---
+++ test/app.js
@@ -0,0 +1,17 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect
+const baseUrl = "localhost:8080"
+
+chai.use(chaiHttp);
+describe("Checking Server", function(){
+    it('Server is UP', function(done) {
+        chai.request(baseUrl)
+        .get('/')
+        .end(function (err, res) {
+            expect(res).to.have.status(200);
+            expect(res.text).to.equal("Welcome to Topology API");
+            done();
+        });
+    });
+});
